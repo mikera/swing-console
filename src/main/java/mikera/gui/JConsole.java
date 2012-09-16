22916d4beca428b85fc979f440d0c002489b3428
mikera <Mike@Mike-VAIO>
2012-09-16 18:45:19 +0800

    Implement blinking cursor

--- src/main/java/mikera/gui/JConsole.java
+++ src/main/java/mikera/gui/JConsole.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
@@ -16,8 +18,7 @@ import java.io.PrintStream;
 import java.util.Arrays;
 
 import javax.swing.JComponent;
-
-import mikera.gui.Tools;
+import javax.swing.Timer;
 
 
 /**
@@ -32,6 +33,9 @@ public class JConsole extends JComponent {
 	private static final Color DEFAULT_FOREGROUND=Color.LIGHT_GRAY;
 	private static final Color DEFAULT_BACKGROUND=Color.BLACK;
 	private static final Font DEFAULT_FONT=new Font("Courier New", Font.PLAIN, 20);
+	private static final int DEFAULT_BLINKRATE = 200;
+	private static final boolean DEFAULT_BLINK_ON = true;
+
 	
 	private int size;
 	private int rows;
@@ -44,13 +48,61 @@ public class JConsole extends JComponent {
 	private int fontHeight;
 	private int fontYOffset;
 	
+	private boolean cursorVisible=false;
+	private boolean cursorBlinkOn=true;
+	private boolean cursorInverted=true;
+	
 	private int curPosition=0;
 	private Font currentFont=DEFAULT_FONT;
 	private Color curForeground=DEFAULT_FOREGROUND;
 	private Color curBackground=DEFAULT_BACKGROUND;
+	
+	private Timer blinkTimer;
 
 	public JConsole(int columns, int rows) {
 		init(columns,rows);
+		if (DEFAULT_BLINK_ON) {
+			setCursorBlink(true);
+		}
+	}
+	
+	private class TimerAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (cursorBlinkOn) {
+				cursorInverted=!cursorInverted;
+				repaint();
+			} else {
+				blinkTimer.stop();
+				cursorInverted=true;
+			}
+		}		
+	}
+	
+	public void setCursorBlink(boolean blink) {
+		if (blink) {
+			cursorBlinkOn=true;
+			getTimer().start();
+		} else {
+			cursorBlinkOn=false;
+			getTimer().stop();
+			cursorInverted=true;
+		}
+	}
+	
+	public void setBlinkDelay(int millis) {
+		getTimer().setDelay(millis);
+	}
+	
+	private Timer getTimer() {
+		if (blinkTimer==null) {
+			blinkTimer=new Timer(DEFAULT_BLINKRATE, new TimerAction());
+			blinkTimer.setRepeats(true);
+			if (cursorBlinkOn) {
+				blinkTimer.start();
+			}
+		}
+		return blinkTimer;
 	}
 	
 	public  void setRows(int rows) {
@@ -59,6 +111,10 @@ public class JConsole extends JComponent {
 	
 	public void setFont(Font f) {
 		currentFont=f;
+	}
+	
+	public void setCursorVisible(boolean visible) {
+		cursorVisible=visible;
 	}
 
 	public int getRows() {
@@ -106,27 +162,43 @@ public class JConsole extends JComponent {
         //textTransform.scale(fontWidth, fontHeight);
         //g.setTransform(textTransform);
 		
+        // calculate x and y range to redraw
         int x1=(int)(r.getMinX()/fontWidth);
         int x2=(int)(r.getMaxX()/fontWidth)+1;
         int y1=(int)(r.getMinY()/fontWidth);
         int y2=(int)(r.getMaxY()/fontWidth)+1;
         
-        for(int j = Math.max(0,y1); j < Math.min(y2,rows); j++) {
+		int curX=getCursorX();
+		int curY=getCursorY();
+
+		for(int j = Math.max(0,y1); j < Math.min(y2,rows); j++) {
     		int offset=j*columns;
     		int start=Math.max(x1, 0);
     		int end=Math.min(x2,columns);
+    		
         	while (start<end) {
         		Color nfg=foreground[offset+start];
         		Color nbg=background[offset+start];
         		Font nf=font[offset+start];
-        		
-        		// detect run
+  
+        		// index of ending position
         		int i=start+1;
-        		while ((i<end)
-        				&&(nfg==foreground[offset+i])
-        				&&(nbg==background[offset+i])
-        				&&(nf==font[offset+i])) {
-        			i++;
+
+        		if ((j==curY)&&(start==curX)) {
+        			if (cursorVisible&&cursorBlinkOn&&cursorInverted) {
+        				// swap foreground and background colours
+        				Color t=nfg;
+        				nfg=nbg;
+        				nbg=t;
+        			}
+        		} else {	
+	        		// detect run
+	        		while ((i<end)&&(!((j==curY)&&(i==curX)))
+	        				&&(nfg==foreground[offset+i])
+	        				&&(nbg==background[offset+i])
+	        				&&(nf==font[offset+i])) {
+	        			i++;
+	        		}
         		}
 
         		// set font
--- src/main/java/mikera/gui/demo/ConsoleApp.java
+++ src/main/java/mikera/gui/demo/ConsoleApp.java
@@ -9,14 +9,20 @@ public class ConsoleApp {
 
     public static void main(String[] args) {	
     	JConsole jc=new JConsole(80,25);
+    	jc.setCursorVisible(true);
+    	jc.setCursorBlink(true);
     	jc.write("Hello World\n");
     	jc.write("Hello World\n",Color.BLACK,Color.MAGENTA);
     	jc.write("Hello World\n",Color.GREEN,Color.BLACK);
     	//System.out.println("Normal output");
     	jc.captureStdOut();
+    	
+    	
+    	// brown box
     	jc.fillArea(' ', Color.WHITE, new Color(100,70,30), 20, 20, 3, 3);
     	
-    	System.out.println("Hello StdOut");
+    	jc.setCursorPos(0, 0);
+
     	Tools.showFillingComponent(jc);
     	
     }
