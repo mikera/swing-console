24ca8e3eb04d9013102c79903dfe65ef1d7c6780
mikera <mike.r.anderson.13@gmail.com>
2012-09-23 16:46:31 +0800

    Add some basic test cases

---
+++ src/test/java/mikera/gui/ConsoleDataTest.java
@@ -0,0 +1,29 @@
+package mikera.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConsoleDataTest {
+	
+	@Test public void testCreateData() {
+		ConsoleData cd=new ConsoleData();
+		cd.init(80, 25);
+		
+		// should be filled with zero chars
+		assertEquals(0,cd.getCharAt(0, 0));
+	}
+	
+	@Test public void testFill() {
+		ConsoleData cd=new ConsoleData();
+		cd.init(80, 25);
+		
+		cd.fillArea('X', null, null, null, 1, 1, 10, 10);
+		
+		// should be filled with zero chars
+		assertEquals(0,cd.getCharAt(0, 0));
+		assertEquals('X',cd.getCharAt(1, 1));
+		assertEquals('X',cd.getCharAt(10, 10));
+		assertEquals(0,cd.getCharAt(11, 11));
+	}
+}
